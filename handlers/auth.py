dd61962a99b215274bcbb122e8ef76b6b744948c
zihad-sudo <zihad@zihadimtiase.com>
2026-01-06 23:29:23 +0600

    Sync handlers/auth.py

---
+++ handlers/auth.py
@@ -0,0 +1,26 @@
+# handlers/auth.py
+import telebot
+from config import ADMIN_PASSWORD
+from utils import add_admin, is_admin
+
+def register_auth_handlers(bot):
+    @bot.message_handler(commands=['admin_login'])
+    def login(message):
+        user_id = message.from_user.id
+        text = message.text.split()
+        
+        if is_admin(user_id):
+            bot.reply_to(message, "✅ You are already an Admin.")
+            return
+
+        if len(text) < 2:
+            bot.reply_to(message, "⚠️ Usage: `/admin_login PASSWORD`")
+            return
+            
+        if text[1] == ADMIN_PASSWORD:
+            if add_admin(user_id, bot=bot):
+                bot.reply_to(message, "🎉 <b>Success!</b> You are now an Admin.")
+            else:
+                bot.reply_to(message, "❌ Error.")
+        else:
+            bot.reply_to(message, "⛔ Wrong Password.")
