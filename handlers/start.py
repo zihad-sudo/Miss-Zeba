40c9adffda48ac6a67f0cfb2eb3c335b9103f20b
zihad-sudo <zihad@zihadimtiase.com>
2026-01-06 23:52:26 +0600

    Sync handlers/start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,11 +1,16 @@
 # handlers/start.py
 from keyboards.main_menu import main_menu
-from utils import get_text
+from utils import get_text, track_user
 
 def register_start(bot):
     @bot.message_handler(commands=["start"])
     def start(message):
         user_id = message.from_user.id
+        
+        # 1. Track the user (Saves to users.json)
+        track_user(message.from_user)
+        
+        # 2. Send Welcome
         welcome_text = get_text("start_message", "👋 Welcome!")
         
         bot.send_message(
