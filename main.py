610723a5588d5bc8e9aece2d88f5e7802c6fc377
zihad-sudo <zihad@zihadimtiase.com>
2026-01-06 22:48:34 +0600

    Sync main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import telebot
 from config import BOT_TOKEN
 from handlers.start import register_start
 from handlers.callbacks import register_callbacks
+from handlers.admin_panel import register_admin_handlers # <--- Import this
 
 if not BOT_TOKEN:
     raise RuntimeError("BOT_TOKEN is missing")
@@ -9,7 +10,8 @@ if not BOT_TOKEN:
 bot = telebot.TeleBot(BOT_TOKEN, parse_mode="HTML")
 
 register_start(bot)
+register_admin_handlers(bot) # <--- Register BEFORE callbacks to ensure priority
 register_callbacks(bot)
 
-print("🤖 Bot is running successfully...")
+print("🤖 Bot is running...")
 bot.infinity_polling()
