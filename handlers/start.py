bbab2b2f1689d0fe8880bc957479ffeed058ef34
zihad-sudo <zihad@zihadimtiase.com>
2026-01-06 23:29:19 +0600

    Sync handlers/start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,12 +1,15 @@
-#handelers/start.py
+# handlers/start.py
 from keyboards.main_menu import main_menu
 from utils import get_text
 
 def register_start(bot):
     @bot.message_handler(commands=["start"])
     def start(message):
+        user_id = message.from_user.id
+        welcome_text = get_text("start_message", "👋 Welcome!")
+        
         bot.send_message(
             message.chat.id,
-            get_text("start_message", "<b>Welcome Dummy Dummy</b>\n\n<i>Ajaira Khaia Kam naiga!</i>"),
-            reply_markup=main_menu()
+            welcome_text,
+            reply_markup=main_menu(user_id)
         )
