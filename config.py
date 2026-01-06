32405815ec126f2279bdf793db087d1542a5b21a
zihad-sudo <zihad@zihadimtiase.com>
2026-01-06 23:28:59 +0600

    Sync config.py

--- config.py
+++ config.py
@@ -2,4 +2,12 @@ import os
 
 BOT_TOKEN = ("8584791963:AAErer43pnunIri64eLUtyA4kKFco6jGk4o")
 
+# The password to become an admin via /admin_login
+ADMIN_PASSWORD = "Admin" 
+
+# Backup list (Optional)
 SUPER_ADMINS = [7936925985]
+
+# --- NEW: Backup Channel ID ---
+# Make sure the bot is an Admin in this channel/group to send messages!
+BACKUP_CHANNEL_ID = -1003546352030
