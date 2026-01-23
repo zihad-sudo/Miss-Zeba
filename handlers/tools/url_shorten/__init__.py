96b403060f56d89cb684cb65d97e5d11c5d6b0c3
zihad-sudo <zihad@zihadimtiase.com>
2026-01-24 02:24:43 +0600

    Delete handlers directory

--- handlers/tools/url_shorten/__init__.py
+++
@@ -1,4 +0,0 @@
-# handlers/tools/url_shorten/__init__.py
-
-# আমরা ফাইলের নাম 'core.py' দিয়েছি, তাই এখানে .core হবে
-from .core import register_url_handlers
--- handlers/tools/watermark/__init__.py
+++
@@ -1,2 +0,0 @@
-from .core import register_watermark_handlers
-__all__ = ['register_watermark_handlers']
